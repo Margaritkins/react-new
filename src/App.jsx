0a0dced881552fd3bc0cba5463ed08080c1e7794
Margarita <te120883ama@gmail.com>
2023-09-11 22:26:50 +0200

    Add Note with children props

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 import "./App.css";
+import Note from "./components/Note";
+import {COLORS} from'./constants';
 
-const List = ({title, children}) => {
-  return (
-    <article>
-      <h3 title={title}>list {title}</h3>
-      <ul>{children}</ul>
-    </article>
-  );
-};
+
+// const List = ({title, children}) => {
+//   return (
+//     <article>
+//       <h3 title={title}>list {title}</h3>
+//       <ul>{children}</ul>
+//     </article>
+//   );
+// };
 
 function App() {
   return (
     <>
-      <List title="first">
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-      </List>
-      <List title="second">
-        <li>10</li>
-        <li>20</li>
-        <li>30</li>
-        
-      </List>
+    <Note title = 'Pitfall' bgColor = {COLORS.PITFALL}>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, esse.</p>
+    </Note>
+    <Note>
+<p><em>Lorem ipsum dolor sit amet consectetur adipisicing elit. </em>Veniam perferendis officia mollitia nostrum provident. Ullam accusamus eligendi quas alias, laudantium quaerat saepe. Aliquam molestias enim recusandae. Ducimus cum in numquam doloremque quod. Harum ab delectus officiis facilis dolorum omnis perspiciatis?
+</p>
+    </Note>
+    <Note title = 'Depricated' bgColor = {COLORS.DEPRICATE}>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, esse.</p>
+    </Note>
     </>
   );
 }
