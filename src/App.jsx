1609e8d07b34797de9d6cfb84365c10dc02894ed
Margarita <te120883ama@gmail.com>
2023-09-11 21:28:31 +0200

    Use shildren props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import "./App.css";
-import CiaoSection from "./components/CiaoSection";
-// import CardSection from "./components/CardSection";
+
+const List = ({title, children}) => {
+  return (
+    <article>
+      <h3 title={title}>list {title}</h3>
+      <ul>{children}</ul>
+    </article>
+  );
+};
 
 function App() {
   return (
     <>
-    {/* <CardSection/> */}
-      <CiaoSection />
+      <List title="first">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </List>
+      <List title="second">
+        <li>10</li>
+        <li>20</li>
+        <li>30</li>
+        
+      </List>
     </>
   );
 }
