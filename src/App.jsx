50212801b00be11713113ded228254934434fb23
Margarita <te120883ama@gmail.com>
2023-09-14 23:37:02 +0200

    Add BrowserRouter & NavMenu

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import UsersLoader from "./components/UsersLoader";
-// import Note from "./components/Note";
-import { COLORS, FLEX } from "./constants";
-
-import FlexContainer from "./components/FlexContainer";
+import NavMenu from "./components/NavMenu";
 
 function App() {
-  
   return (
     <>
-      <UsersLoader/>
+      {/* <UsersLoader/> */}
+      <BrowserRouter>
+        <header>
+          <NavMenu />
+        </header>
+        
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
+          </Routes>
+        </main>
 
+        <footer>&copy;2023</footer>
+      </BrowserRouter>
     </>
   );
 }
 
+const Home = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
+const Contacts = () => <h1>Contacts</h1>;
 export default App;
